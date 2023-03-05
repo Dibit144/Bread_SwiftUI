c2345aa5c080c5e491ea4ea04cad2a8a0e25cdc1
David Collett <davidcollett@fastmail.com.au>
2023-03-05 16:06:25 +1100

    Initial Commit

---
+++ Bread_SwiftUI/Bread_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bread_SwiftUIApp.swift
+//  Bread_SwiftUI
+//
+//  Created by David Collett on 5/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Bread_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
